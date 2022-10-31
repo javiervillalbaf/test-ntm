b6a84362ec2c0b86883a44d52dc7f6f0086a4cec
Javier Villalba <javiervillalba.04@gmail.com>
2022-10-31 17:35:48 -0300

    funionalities

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let arrow = document.getElementsByClassName("arrow");
 let deviceDown = document.getElementById("deviceDown");
 let btn = document.getElementById("btn");
+let allContainer = document.getElementById("allContainer");
 let circleOne = document.getElementById("circleOne");
 let circleTwo = document.getElementById("circleTwo");
 let circleThree = document.getElementById("circleThree");
@@ -23,6 +24,74 @@ let lineFive = document.getElementById("lineFive");
 let lineSix = document.getElementById("lineSix");
 let lineSeven = document.getElementById("lineSeven");
 
+function marginBot() {
+  if (allContainer.classList.contains("all-container-active") == false) {
+    allContainer.classList.toggle("all-container-active");
+  } else if (
+    containerTopOne.classList.contains("container-active") == false &&
+    containerTopTwo.classList.contains("container-active") == false &&
+    containerBotOne.classList.contains("container-active") == false &&
+    containerBotTwo.classList.contains("container-active") == false &&
+    containerBotThree.classList.contains("container-active") == false &&
+    containerBotFour.classList.contains("container-active") == false &&
+    containerBotFive.classList.contains("container-active") == false
+  ) {
+    allContainer.classList.toggle("all-container-active");
+  }
+}
+
+function closeTexts() {
+  if (
+    containerTopOne.classList.contains("container-active") ||
+    lineOne.classList.contains("line-one-active")
+  ) {
+    containerTopOne.classList.toggle("container-active");
+    lineOne.classList.toggle("line-one-active");
+  }
+  if (
+    containerTopTwo.classList.contains("container-active") ||
+    lineTwo.classList.contains("line-two-active")
+  ) {
+    containerTopTwo.classList.toggle("container-active");
+    lineTwo.classList.toggle("line-two-active");
+  }
+  if (
+    containerBotOne.classList.contains("container-active") ||
+    lineThree.classList.contains("line-three-active")
+  ) {
+    containerBotOne.classList.toggle("container-active");
+    lineThree.classList.toggle("line-three-active");
+  }
+  if (
+    containerBotTwo.classList.contains("container-active") ||
+    lineFour.classList.contains("line-four-active")
+  ) {
+    containerBotTwo.classList.toggle("container-active");
+    lineFour.classList.toggle("line-four-active");
+  }
+  if (
+    containerBotThree.classList.contains("container-active") ||
+    lineFive.classList.contains("line-five-active")
+  ) {
+    containerBotThree.classList.toggle("container-active");
+    lineFive.classList.toggle("line-five-active");
+  }
+  if (
+    containerBotFour.classList.contains("container-active") ||
+    lineSix.classList.contains("line-six-active")
+  ) {
+    containerBotFour.classList.toggle("container-active");
+    lineSix.classList.toggle("line-six-active");
+  }
+  if (
+    containerBotFive.classList.contains("container-active") ||
+    lineSeven.classList.contains("line-seven-active")
+  ) {
+    containerBotFive.classList.toggle("container-active");
+    lineSeven.classList.toggle("line-seven-active");
+  }
+}
+
 btn.addEventListener("click", () => {
   deviceDown.classList.toggle("inactive");
   deviceDown.classList.toggle("device-down");
@@ -34,11 +103,14 @@ btn.addEventListener("click", () => {
   circleFive.classList.toggle("disable");
   circleSix.classList.toggle("disable");
   circleSeven.classList.toggle("disable");
+  closeTexts();
 });
+
 circleOne.addEventListener("click", () => {
   containerTopOne.classList.toggle("container-active");
   lineOne.classList.toggle("line-one-active");
 });
+
 circleTwo.addEventListener("click", () => {
   containerTopTwo.classList.toggle("container-active");
   lineTwo.classList.toggle("line-two-active");
@@ -50,16 +122,337 @@ circleThree.addEventListener("click", () => {
 circleFour.addEventListener("click", () => {
   containerBotTwo.classList.toggle("container-active");
   lineFour.classList.toggle("line-four-active");
+  marginBot();
 });
 circleFive.addEventListener("click", () => {
   containerBotThree.classList.toggle("container-active");
   lineFive.classList.toggle("line-five-active");
+  marginBot();
 });
 circleSix.addEventListener("click", () => {
   containerBotFour.classList.toggle("container-active");
   lineSix.classList.toggle("line-six-active");
+  marginBot();
 });
 circleSeven.addEventListener("click", () => {
   containerBotFive.classList.toggle("container-active");
   lineSeven.classList.toggle("line-seven-active");
+  marginBot();
 });
+
+const mobbileBP = matchMedia("(max-width:768px)");
+const changeMobile = (mql) => {
+  if (mql.matches) {
+    closeTexts();
+    circleOne.addEventListener("click", () => {
+      if (
+        containerTopTwo.classList.contains("container-active") ||
+        lineTwo.classList.contains("line-two-active")
+      ) {
+        containerTopTwo.classList.toggle("container-active");
+        lineTwo.classList.toggle("line-two-active");
+      }
+      if (
+        containerBotOne.classList.contains("container-active") ||
+        lineThree.classList.contains("line-three-active")
+      ) {
+        containerBotOne.classList.toggle("container-active");
+        lineThree.classList.toggle("line-three-active");
+      }
+      if (
+        containerBotTwo.classList.contains("container-active") ||
+        lineFour.classList.contains("line-four-active")
+      ) {
+        containerBotTwo.classList.toggle("container-active");
+        lineFour.classList.toggle("line-four-active");
+      }
+      if (
+        containerBotThree.classList.contains("container-active") ||
+        lineFive.classList.contains("line-five-active")
+      ) {
+        containerBotThree.classList.toggle("container-active");
+        lineFive.classList.toggle("line-five-active");
+      }
+      if (
+        containerBotFour.classList.contains("container-active") ||
+        lineSix.classList.contains("line-six-active")
+      ) {
+        containerBotFour.classList.toggle("container-active");
+        lineSix.classList.toggle("line-six-active");
+      }
+      if (
+        containerBotFive.classList.contains("container-active") ||
+        lineSeven.classList.contains("line-seven-active")
+      ) {
+        containerBotFive.classList.toggle("container-active");
+        lineSeven.classList.toggle("line-seven-active");
+      }
+    });
+    circleTwo.addEventListener("click", () => {
+      if (
+        containerTopOne.classList.contains("container-active") ||
+        lineOne.classList.contains("line-one-active")
+      ) {
+        containerTopOne.classList.toggle("container-active");
+        lineOne.classList.toggle("line-one-active");
+      }
+      if (
+        containerBotOne.classList.contains("container-active") ||
+        lineThree.classList.contains("line-three-active")
+      ) {
+        containerBotOne.classList.toggle("container-active");
+        lineThree.classList.toggle("line-three-active");
+      }
+      if (
+        containerBotTwo.classList.contains("container-active") ||
+        lineFour.classList.contains("line-four-active")
+      ) {
+        containerBotTwo.classList.toggle("container-active");
+        lineFour.classList.toggle("line-four-active");
+      }
+      if (
+        containerBotThree.classList.contains("container-active") ||
+        lineFive.classList.contains("line-five-active")
+      ) {
+        containerBotThree.classList.toggle("container-active");
+        lineFive.classList.toggle("line-five-active");
+      }
+      if (
+        containerBotFour.classList.contains("container-active") ||
+        lineSix.classList.contains("line-six-active")
+      ) {
+        containerBotFour.classList.toggle("container-active");
+        lineSix.classList.toggle("line-six-active");
+      }
+      if (
+        containerBotFive.classList.contains("container-active") ||
+        lineSeven.classList.contains("line-seven-active")
+      ) {
+        containerBotFive.classList.toggle("container-active");
+        lineSeven.classList.toggle("line-seven-active");
+      }
+    });
+    circleThree.addEventListener("click", () => {
+      if (
+        containerTopTwo.classList.contains("container-active") ||
+        lineTwo.classList.contains("line-two-active")
+      ) {
+        containerTopTwo.classList.toggle("container-active");
+        lineTwo.classList.toggle("line-two-active");
+      }
+      if (
+        containerTopOne.classList.contains("container-active") ||
+        lineOne.classList.contains("line-one-active")
+      ) {
+        containerTopOne.classList.toggle("container-active");
+        lineOne.classList.toggle("line-one-active");
+      }
+      if (
+        containerBotTwo.classList.contains("container-active") ||
+        lineFour.classList.contains("line-four-active")
+      ) {
+        containerBotTwo.classList.toggle("container-active");
+        lineFour.classList.toggle("line-four-active");
+      }
+      if (
+        containerBotThree.classList.contains("container-active") ||
+        lineFive.classList.contains("line-five-active")
+      ) {
+        containerBotThree.classList.toggle("container-active");
+        lineFive.classList.toggle("line-five-active");
+      }
+      if (
+        containerBotFour.classList.contains("container-active") ||
+        lineSix.classList.contains("line-six-active")
+      ) {
+        containerBotFour.classList.toggle("container-active");
+        lineSix.classList.toggle("line-six-active");
+      }
+      if (
+        containerBotFive.classList.contains("container-active") ||
+        lineSeven.classList.contains("line-seven-active")
+      ) {
+        containerBotFive.classList.toggle("container-active");
+        lineSeven.classList.toggle("line-seven-active");
+      }
+    });
+    circleFour.addEventListener("click", () => {
+      if (
+        containerTopTwo.classList.contains("container-active") ||
+        lineTwo.classList.contains("line-two-active")
+      ) {
+        containerTopTwo.classList.toggle("container-active");
+        lineTwo.classList.toggle("line-two-active");
+      }
+      if (
+        containerTopOne.classList.contains("container-active") ||
+        lineOne.classList.contains("line-one-active")
+      ) {
+        containerTopOne.classList.toggle("container-active");
+        lineOne.classList.toggle("line-one-active");
+      }
+      if (
+        containerBotOne.classList.contains("container-active") ||
+        lineThree.classList.contains("line-three-active")
+      ) {
+        containerBotOne.classList.toggle("container-active");
+        lineThree.classList.toggle("line-three-active");
+      }
+      if (
+        containerBotThree.classList.contains("container-active") ||
+        lineFive.classList.contains("line-five-active")
+      ) {
+        containerBotThree.classList.toggle("container-active");
+        lineFive.classList.toggle("line-five-active");
+      }
+      if (
+        containerBotFour.classList.contains("container-active") ||
+        lineSix.classList.contains("line-six-active")
+      ) {
+        containerBotFour.classList.toggle("container-active");
+        lineSix.classList.toggle("line-six-active");
+      }
+      if (
+        containerBotFive.classList.contains("container-active") ||
+        lineSeven.classList.contains("line-seven-active")
+      ) {
+        containerBotFive.classList.toggle("container-active");
+        lineSeven.classList.toggle("line-seven-active");
+      }
+    });
+    circleFive.addEventListener("click", () => {
+      if (
+        containerTopTwo.classList.contains("container-active") ||
+        lineTwo.classList.contains("line-two-active")
+      ) {
+        containerTopTwo.classList.toggle("container-active");
+        lineTwo.classList.toggle("line-two-active");
+      }
+      if (
+        containerTopOne.classList.contains("container-active") ||
+        lineOne.classList.contains("line-one-active")
+      ) {
+        containerTopOne.classList.toggle("container-active");
+        lineOne.classList.toggle("line-one-active");
+      }
+      if (
+        containerBotOne.classList.contains("container-active") ||
+        lineThree.classList.contains("line-three-active")
+      ) {
+        containerBotOne.classList.toggle("container-active");
+        lineThree.classList.toggle("line-three-active");
+      }
+      if (
+        containerBotTwo.classList.contains("container-active") ||
+        lineFour.classList.contains("line-four-active")
+      ) {
+        containerBotTwo.classList.toggle("container-active");
+        lineFour.classList.toggle("line-four-active");
+      }
+      if (
+        containerBotFour.classList.contains("container-active") ||
+        lineSix.classList.contains("line-six-active")
+      ) {
+        containerBotFour.classList.toggle("container-active");
+        lineSix.classList.toggle("line-six-active");
+      }
+      if (
+        containerBotFive.classList.contains("container-active") ||
+        lineSeven.classList.contains("line-seven-active")
+      ) {
+        containerBotFive.classList.toggle("container-active");
+        lineSeven.classList.toggle("line-seven-active");
+      }
+    });
+    circleSix.addEventListener("click", () => {
+      if (
+        containerTopTwo.classList.contains("container-active") ||
+        lineTwo.classList.contains("line-two-active")
+      ) {
+        containerTopTwo.classList.toggle("container-active");
+        lineTwo.classList.toggle("line-two-active");
+      }
+      if (
+        containerTopOne.classList.contains("container-active") ||
+        lineOne.classList.contains("line-one-active")
+      ) {
+        containerTopOne.classList.toggle("container-active");
+        lineOne.classList.toggle("line-one-active");
+      }
+      if (
+        containerBotOne.classList.contains("container-active") ||
+        lineThree.classList.contains("line-three-active")
+      ) {
+        containerBotOne.classList.toggle("container-active");
+        lineThree.classList.toggle("line-three-active");
+      }
+      if (
+        containerBotTwo.classList.contains("container-active") ||
+        lineFour.classList.contains("line-four-active")
+      ) {
+        containerBotTwo.classList.toggle("container-active");
+        lineFour.classList.toggle("line-four-active");
+      }
+      if (
+        containerBotThree.classList.contains("container-active") ||
+        lineFive.classList.contains("line-five-active")
+      ) {
+        containerBotThree.classList.toggle("container-active");
+        lineFive.classList.toggle("line-five-active");
+      }
+      if (
+        containerBotFive.classList.contains("container-active") ||
+        lineSeven.classList.contains("line-seven-active")
+      ) {
+        containerBotFive.classList.toggle("container-active");
+        lineSeven.classList.toggle("line-seven-active");
+      }
+    });
+    circleSeven.addEventListener("click", () => {
+      if (
+        containerTopTwo.classList.contains("container-active") ||
+        lineTwo.classList.contains("line-two-active")
+      ) {
+        containerTopTwo.classList.toggle("container-active");
+        lineTwo.classList.toggle("line-two-active");
+      }
+      if (
+        containerTopOne.classList.contains("container-active") ||
+        lineOne.classList.contains("line-one-active")
+      ) {
+        containerTopOne.classList.toggle("container-active");
+        lineOne.classList.toggle("line-one-active");
+      }
+      if (
+        containerBotOne.classList.contains("container-active") ||
+        lineThree.classList.contains("line-three-active")
+      ) {
+        containerBotOne.classList.toggle("container-active");
+        lineThree.classList.toggle("line-three-active");
+      }
+      if (
+        containerBotTwo.classList.contains("container-active") ||
+        lineFour.classList.contains("line-four-active")
+      ) {
+        containerBotTwo.classList.toggle("container-active");
+        lineFour.classList.toggle("line-four-active");
+      }
+      if (
+        containerBotThree.classList.contains("container-active") ||
+        lineFive.classList.contains("line-five-active")
+      ) {
+        containerBotThree.classList.toggle("container-active");
+        lineFive.classList.toggle("line-five-active");
+      }
+      if (
+        containerBotFour.classList.contains("container-active") ||
+        lineSix.classList.contains("line-six-active")
+      ) {
+        containerBotFour.classList.toggle("container-active");
+        lineSix.classList.toggle("line-six-active");
+      }
+    });
+  }
+};
+mobbileBP.addEventListener("change", changeMobile);
+changeMobile(mobbileBP);
